2ffba697a7a4fe4544a12fb29be0a7d709dfba4b
shone <shonejolly98@gmail.com>
2025-08-05 22:31:03 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from ImageForgeryDetection import logger
+from ImageForgeryDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome to Image Forgery Detection')
+STAGE_NAME = "Data Ingestion stage"
+
+if __name__=='__main__':
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
