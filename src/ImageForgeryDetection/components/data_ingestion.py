ca7c8a5292d6899a1d92073c7592abee65a5154c
shone <shonejolly98@gmail.com>
2025-08-10 18:31:29 +0530

    dvc added

--- src/ImageForgeryDetection/components/data_ingestion.py
+++ src/ImageForgeryDetection/components/data_ingestion.py
@@ -35,19 +35,9 @@ class DataIngestion:
     
     def extract_zip_file(self):
         """
-        Extracts the zip file into the data directory and deletes it
+        Extracts the zip file into the data directory
         """
         unzip_path = self.config.unzip_dir
-        zip_file_path = self.config.local_data_file
-        logger.info(f"Extracting {zip_file_path} to {unzip_path}")
-        try:
-            os.makedirs(unzip_path, exist_ok=True)
-            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-                zip_ref.extractall(unzip_path)
-            logger.info(f"Extracted to {unzip_path}")
-            logger.info(f"Deleting {zip_file_path}")
-            os.remove(zip_file_path)
-            logger.info(f"Deleted {zip_file_path}")
-        except Exception as e:
-            logger.error(f"Error extracting/deleting {zip_file_path}: {e}")
-            raise
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
